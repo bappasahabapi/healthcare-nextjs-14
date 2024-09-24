5e73c747fbfa3704990f760a85fcec1eba39b4f9
Bappa Saha <bappasaha161@gmail.com>
2024-09-24 21:23:27 +0600

    8-Install redux-toolkit

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,14 +1,20 @@
+// import {reducer} from './rootReducer'
 import { configureStore } from '@reduxjs/toolkit'
-import {reducer} from './rootReducer'
+import { baseApi } from './api/baseApi'
 
-// export const store = configureStore({
-//   reducer: {},
-// })
+export const store = configureStore({
+  reducer: {
+    [baseApi.reducerPath]:baseApi.reducer
+  },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(baseApi.middleware),
+})
 
 //todo: we can make a root reducer and also do the same things 
-export const store = configureStore({
-    reducer,
-  })
+// export const store = configureStore({
+//     reducer,
+//   })
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
